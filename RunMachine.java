6f3cf3f51d27943a40c5da2830c3760cd0143bbd
Eugen2222 <51314662+Eugen2222@users.noreply.github.com>
2020-01-05 23:53:15 +0000

    Revise the remark

--- RunMachine.java
+++ RunMachine.java
@@ -1,11 +1,7 @@
 
 public class RunMachine {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Create a fruit model
 		String path = "/d/v.csv";
-		VController v = new VController(path);
-		//Create a fruit controller to allow user interaction 
-		//with a viewer window inside to display the game.
+		VController v = new VController(path); // build up a controller. 
 	}
 }
